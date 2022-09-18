b92b8cad3a6e0911c45be4230598e598a3cb2cef
Yasin Mamedov <93099463+chousouu@users.noreply.github.com>
2022-09-18 21:39:47 +0300

    Update Onegin.h

--- Onegin.h
+++ Onegin.h
@@ -7,7 +7,7 @@
 #include <ctype.h>
 #include <sys\stat.h>
 
-int AnalyzeInput(int argcount, const char **argument, struct Input *info);
+int ParseArguments(int argcount, const char **argument, struct Input *info);
 void OddSpaceRemoveArray(char *buffer);
 char *ReadToBuffer(FILE *fp, int size);
 void GetString(char *buffer, struct Strings *arr, int size);
@@ -17,9 +17,9 @@ Strings *FillInStruct(char *buffer, int size);
 int Compare(const void *s1, const void *s2);
 int CompareFromEnd(const void *s1, const void *s2);
 void bubblesort(struct Strings* p, int sizeofArray, int size, int (*Comp)(const void *z1, const void *z2));
-void quick_sort(struct Strings* p, int left, int right,  int (*CompareFromEnd)(const void *z1, const void *z2)); 
-void swap(struct Strings *s1, struct Strings *s2);
-int LowerCase(char c);
+void QuickSort(struct Strings* p, int left, int right,  int (*CompareFromEnd)(const void *z1, const void *z2)); 
+void SwapStruct(struct Strings *s1, struct Strings *s2);
+char LowerCase(char c);
 void PrintStrings(struct Strings *arr, int size, const char *name, int mode);
 char *OpenFile(const char *name);
 void FreeMemory(struct Strings *arr);
